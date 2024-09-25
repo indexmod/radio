191d024550b5eae8ed641dfb8e741803b14510cb
altai2112 <23720911+indexmod@users.noreply.github.com>
2024-09-25 19:28:45 +0300

    обновление скрипта и камо

--- assets/js/program-audio.js
+++ assets/js/program-audio.js
@@ -1,17 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
   var audio = document.getElementById('audio-player');
 
-  // Обработка кликов для настольных устройств
-  document.addEventListener('click', function() {
+  // Функция для воспроизведения и паузы аудио
+  function toggleAudio() {
     if (audio.paused) {
       audio.play();
+    } else {
+      audio.pause();
     }
+  }
+
+  // Обработка кликов для настольных устройств
+  document.addEventListener('click', function() {
+    toggleAudio();
   });
 
   // Обработка нажатий на сенсорных устройствах
   document.addEventListener('touchstart', function() {
-    if (audio.paused) {
-      audio.play();
+    toggleAudio();
+  });
+
+  // Обработка нажатий клавиш
+  document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+      event.preventDefault(); // Предотвращает прокрутку страницы
+      toggleAudio();
     }
   });
 });
